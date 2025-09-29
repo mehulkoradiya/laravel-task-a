fa1acdcfb876a6eb64a60e77030c9abb2c1244a8
mehulkoradiya <koradiyamehul@gmail.com>
2025-09-29 15:27:17 +0530

    chunk image upload

--- routes/api.php
+++ routes/api.php
@@ -12,8 +12,3 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/products/import', [ImportController::class, 'uploadCsv']);
 Route::get('/products/import/{id}/status', [ImportController::class, 'status']);
-
-Route::post('/uploads/initiate', [UploadController::class, 'initiate']);
-Route::post('/uploads/{uuid}/chunk', [UploadController::class, 'uploadChunk']);
-Route::get('/uploads/{uuid}/status', [UploadController::class, 'status']);
-Route::post('/uploads/{uuid}/complete', [UploadController::class, 'complete']);
